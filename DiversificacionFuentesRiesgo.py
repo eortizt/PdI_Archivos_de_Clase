16ecdb196679218095395d05479c8663d051dc2d
Unknown <if708924@iteso.mx>
2018-09-24 12:49:48 -0500

    Fin clase 10
    Se analiza la frontera de minima varianza para portafolios de 2 y 3 acctivos, generar codigo para n activos, hay que modificar la ultima parte del codigo

--- DiversificacionFuentesRiesgo.py
+++ DiversificacionFuentesRiesgo.py
@@ -47,6 +47,7 @@ r12 = corr['EU']['Japon']
 # Crear un DataFrame cuyas columnas sean rendimiento
 # y volatilidad del portafolio para cada una de las w
 # generadas
+# se llama portafolios2 porque son 2 activos
 portafolios2 = pd.DataFrame(index=w,columns=['Rendimiento','Volatilidad'])
 portafolios2.index.name = 'W'
 portafolios2.Rendimiento = w*E1+(1-w)*E2
@@ -77,6 +78,131 @@ plt.show()
 
 #%%  ¿Cómo hallar el portafolio de varianza mínima?
 # El minimo de la función de volatilidad vs rendimiento, la derivada evaluada en 0
-
+# var_p = w1**2*s1**2+(1-w1)**2*s2**2+2*w1(1-w)*s12
+# se deriva, se iguala a cero y se despeja w, esa w es w_minvar
 w_minvar = (S2**2-r12*S1*S2)/(S1**2+S2**2-2*r12*S1*S2)
 w_minvar
+
+
+#%% Con scipy.optimize
+import scipy.optimize as opt
+# Función objetivo
+def var2(w,s1,s2,s12):
+    return (w*s1)**2+(1-w)**2*s2**2+2*w*(1-w)*s12
+# Dato inicial
+w0 = 0
+# Volatilidades individuales y covarianza
+s1 = annual_ret_summ.EU['Volatilidad']
+s2 = annual_ret_summ.Japon['Volatilidad']
+s12 = corr['EU']['Japon']*s1*s2
+
+# Cota de w
+bnd = (0,1)
+
+# Solución
+min_var_2 = opt.minimize(var2, w0, args=(s1,s2,s12),bounds=(bnd,))
+min_var_2
+
+#%%
+# Graficar el portafolio de varianza mínima
+# sobre el mismo gráfico realizado anteriormente
+plt.figure(figsize=(8,6))
+plt.plot(S1,E1,'ro',ms=10, label='EU')
+plt.plot(S2,E2,'bo',ms=10, label='Japon')
+plt.plot(portafolios2.Volatilidad,portafolios2.Rendimiento,'k-',lw=4,label='Portafolios')
+plt.plot(np.sqrt(min_var_2.fun),min_var_2.x*E1+(1-min_var_2.x)*E2,'mo',ms=10,label='Por_Min_var')
+plt.xlabel('Volatilidad $sigma$')
+plt.ylabel('Rendimiento $E[r]$')
+plt.legend(loc='best')
+plt.grid()
+plt.show()
+
+
+#%% para tres activos, obtengamos la frontera de mínima varianza
+## Construcción de parámetros
+
+# 1. Sigma: matriz de varianza-covarianza
+#       cov_mat
+s1 = annual_ret_summ['EU']['Volatilidad']
+s2 = annual_ret_summ['Japon']['Volatilidad']
+s3 = annual_ret_summ['RU']['Volatilidad']
+s12 = corr['EU']['Japon']*s1*s2
+s13 = corr['EU']['RU']*s1*s3
+s23 = corr['Japon']['RU']*s2*s3
+Sigma = np.array([[s1**2, s12, s13],
+                  [s12, s2**2, s23],
+                  [s13, s23, s3**2]])
+    
+    
+# 2. Eind: rendimientos esperados activos individuales
+#       es un vector con los rendimientos esperados individuales   Eind = np.array([E1, E2, E3])
+E1 = annual_ret_summ['EU']['Media']
+E2 = annual_ret_summ['Japon']['Media']
+E3 = annual_ret_summ['RU']['Media']
+Eind = np.array([E1, E2, E3])
+
+
+# 3. Ereq: rendimientos requeridos para el portafolio
+#       # Número de portafolios
+N = 100
+Ereq = np.linspace(Eind.min(), Eind.max(), N)
+
+def varianza(w, Sigma):
+    return w.dot(Sigma).dot(w)
+def rendimiento_req(w, Eind, Ereq):
+    return Eind.dot(w)-Ereq
+
+
+# Dato inicial
+w0 = np.zeros(3,)
+# Cotas de las variables
+bnds = ((0,None), (0,None), (0,None)) # todas tienen que ser positivas
+
+
+
+# DataFrame de portafolios de la frontera
+portfolios3 = pd.DataFrame(index=range(N), columns=['w1', 'w2', 'w3', 'Ret', 'Vol'])
+
+# Construcción de los N portafolios de la frontera
+for i in range(N):
+    # Restricciones
+    cons = ({'type': 'eq', 'fun': rendimiento_req, 'args': (Eind,Ereq[i])},
+            {'type': 'eq', 'fun': lambda w: np.sum(w)-1})
+    # Portafolio de mínima varianza para nivel de rendimiento esperado Ereq[i]
+    min_var = opt.minimize(varianza, w0, args=(Sigma,), bounds=bnds, constraints=cons)
+    # Pesos, rendimientos y volatilidades de los portafolio
+    portfolios3.loc[i,['w1','w2','w3']] = min_var.x
+    portfolios3['Ret'][i] = rendimiento_req(min_var.x, Eind, Ereq[i])+Ereq[i]
+    portfolios3['Vol'][i] = np.sqrt(varianza(min_var.x, Sigma))
+
+
+plt.figure(figsize=(8,6))
+plt.plot(s1,E1,'ro',ms=5,label='EU')
+plt.plot(s2,E2,'bo',ms=5,label='Japón')
+plt.plot(s3,E3,'co',ms=5,label='RU')
+plt.plot(portfolios3.Vol, portfolios3.Ret, 'k-', lw=4, label='Portafolios 3 act')
+plt.plot(portafolios2.Volatilidad,portafolios2.Rendimiento,'g-',lw=2,label='Portafolios2')
+plt.xlabel('Volatilidad ($\sigma$)')
+plt.ylabel('Rendimiento esperado ($E[r]$)')
+plt.grid()
+#plt.axis([0.14,0.15,0.135,0.142])
+plt.legend(loc='best')
+
+
+# Portafolio de mínima varianza para 3 activos
+cons = ({'type': 'eq', 'fun': lambda w: np.sum(w)-1})
+min_var3 = opt.minimize(varianza, w0, args=(Sigma,), bounds=bnds, constraints=cons)
+
+plt.figure(figsize=(8,6))
+plt.plot(s1,E1,'ro',ms=5,label='EU')
+plt.plot(s2,E2,'bo',ms=5,label='Japón')
+plt.plot(s3,E3,'co',ms=5,label='RU')
+plt.plot(portfolios3.Vol, portfolios3.Ret, 'k-', lw=4, label='Portafolios 3 act')
+plt.plot(portafolios2.Volatilidad,portafolios2.Rendimiento,'g-',lw=2,label='Portafolios2')
+plt.plot(np.sqrt(min_var_2.fun),min_var_2.x*E1+(1-min_var_2.x)*E2,'yo',ms=5,label='Por_Min_var2')
+plt.plot(np.sqrt(min_var3.fun),min_var3.x.dot(Eind),'mo',ms=5,label='Port Min Var 3')
+plt.xlabel('Volatilidad ($\sigma$)')
+plt.ylabel('Rendimiento esperado ($E[r]$)')
+plt.grid()
+#plt.axis([0.14,0.15,0.135,0.142])
+plt.legend(loc='best')
